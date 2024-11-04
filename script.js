9d0d7b77366e2c3012a4e5136bfaae0f637be221
wtc2k2 <wtc2k2@gmail.com>
2024-11-04 14:56:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,7 @@
-function clickme() {
-    var1 = document.getElementById("id1").checked
-    var2 = document.getElementById("id2").checked
-    var3 = document.getElementById("id3").checked
-
-    document.getElementById("div1").textContent = var1
-    document.getElementById("div2").textContent = var2
-    document.getElementById("div3").textContent = var3
-
-    if (var1) {
-        document.getElementById("div4").textContent = "I play sports"
-    }
-    else {
-        document.getElementById("div4").textContent = "I don't play sports"
-    }
+function changeme() {
+    var element = document.getElementById("div1");
+    
+    if (element.innerText !== "Not Here!") {
+        element.innerText = "Not Here!";
+    } 
 }
-
-function selectChange(){
-    var4 = document.getElementById("sports").value
-    document.getElementById("div5").textContent = var4
-}
-
