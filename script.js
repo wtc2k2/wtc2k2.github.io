1578aa841a7c06a9161ed7e2bf8ffb7514ad195d
wtc2k2 <wtc2k2@gmail.com>
2024-12-06 23:25:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+function showthediv(var1) {
+    document.getElementById('div1').style.display = 'none'
+    document.getElementById('div2').style.display = 'none'
+    document.getElementById('div3').style.display = 'none'
+    document.getElementById('div4').style.display = 'none'
+    document.getElementById('div5').style.display = 'none'
+    document.getElementById('div6').style.display = 'none'
+    document.getElementById(var1).style.display = 'block'
+}
